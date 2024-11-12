bbd859955e6233a066ba60616ce977b1f6348738
yook <si89336@naver.com>
2024-11-12 15:48:42 +0900

    demo: update

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,20 +1,20 @@
 module.exports = {
-    ci: {
-        collect: {
-            staticDistDir: './build',
-            url: ['http://localhost:3000'],
-            numberOfRuns: 3,
-        },
-        assert: {
-            assertions: {
-                'categories:performance': ['warn', { minScore: 0.9 }],
-                'categories:accessibility': ['error', { minScore: 1 }],
-            },
-        },
-        upload: {
-            target: 'filesystem',
-            outputDir: './lhci_reports',
-            reportFilenamePattern: '%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%',
-        },
+  ci: {
+    collect: {
+      staticDistDir: "./build",
+      url: ["http://localhost:3000"],
+      numberOfRuns: 3,
     },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 1 }],
+      },
+    },
+    upload: {
+      target: "filesystem",
+      outputDir: "./lhci_reports",
+      reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%",
+    },
+  },
 };
